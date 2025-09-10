fa129d565d7c8908657cb36c390598c349562402
Soumik.Tech <funhub295@gmail.com>
2025-09-11 01:38:02 +0530

    Add files via upload

---
+++ password checker.py
@@ -0,0 +1,26 @@
+# 2.password checker
+
+password = input("Enter the Password: ")
+while len(password) < 8:
+    print("password is too short")
+    password = input("Enter the Password Again: ")
+
+upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lower_case = "abcdefghijklmnopqrstuvwxyz"
+special_chars = "!@#$%^&*()_+"
+numbers = "1234567890"
+
+if any(pswrd.islower() for pswrd in password):
+    print("password has lower case")
+else:
+    print("password does not have lower case")
+if any(pswrd.isupper() for pswrd in password):
+        print("password has upper case")
+else:
+    print("password does not have upper case")
+if any(pswrd in special_chars for pswrd in password):
+        print("password has special char")
+else:
+    print("password does not have special char")
+    pass
+print("\npassword is valid")
